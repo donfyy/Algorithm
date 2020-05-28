4133dfd4fc7ee1686fd0f6aa5f07ac3751b6b024
donfyy <sven.xinxin.wang@gmail.com>
2020-05-28 08:47:04 +0800

    add more

--- Week_02/java/src/_144_BinaryTreePreorderTraversal.java
+++ Week_02/java/src/_144_BinaryTreePreorderTraversal.java
@@ -2,19 +2,26 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
- * 时间:O(n) 空间:O(logn) 最差:O(n)
+ * 第一遍：2020/05/27周三 ✅
+ * 第二遍：2020/05/28周四 ✅
+ * 第三遍：2020/06/03周一
+ * 第四遍：2020/06/17周一
  */
 class _144_BinaryTreePreorderTraversal {
+    /**
+     * 时间:O(n) 空间:O(logn) 最差:O(n)
+     */
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> ret = new LinkedList<>();
         recursive(root, ret);
         return ret;
     }
+
     private void recursive(TreeNode node, List<Integer> ret) {
         if (node == null) {
             return;
         }
-        
+
         ret.add(node.val);
         recursive(node.left, ret);
         recursive(node.right, ret);
@@ -37,6 +44,28 @@ class _144_BinaryTreePreorderTraversal {
 
             stack.offerLast(node.right);
             stack.offerLast(node.left);
+        }
+        return ret;
+    }
+
+    /**
+     * 又是一种思路
+     */
+    public List<Integer> preorderTraversal3(TreeNode root) {
+        List<Integer> ret = new LinkedList<>();
+        LinkedList<TreeNode> stack = new LinkedList<>();
+
+        TreeNode curr = root;
+        while (curr != null || !stack.isEmpty()) {
+            if (curr != null) {
+                ret.add(curr.val);
+
+                stack.offerLast(curr);
+                curr = curr.left;
+            } else {
+                curr = stack.pollLast();
+                curr = curr.right;
+            }
         }
         return ret;
     }
--- Week_02/java/src/_94_BinaryTreeInorderTraversal.java
+++ Week_02/java/src/_94_BinaryTreeInorderTraversal.java
@@ -3,9 +3,15 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
- * 时间：O(n) 空间：O(logn)
+ * 第一遍：2020/05/27周三 ✅
+ * 第二遍：2020/05/28周四 ✅
+ * 第三遍：2020/06/03周一
+ * 第四遍：2020/06/17周一
  */
 class _94_BinaryTreeInorderTraversal {
+    /**
+     * 时间：O(n) 空间：O(logn 最差:O(n))
+     */
     public List<Integer> inorderTraversal(TreeNode root) {
         LinkedList<Integer> ret = new LinkedList<>();
         recursive(root, ret);
@@ -21,6 +27,11 @@ class _94_BinaryTreeInorderTraversal {
         recursive(node.right, ret);
     }
 
+    /**
+     * 需要左根右读值，但最先被访问的是根左。
+     * 根左顺序访问，逆序读值-->先进后出
+     * 时间：O(n) 空间：O(logn 最差:O(n)))
+     */
     public List<Integer> inorderTraversal2(TreeNode root) {
         if (root == null) {
             return Collections.emptyList();
@@ -28,7 +39,7 @@ class _94_BinaryTreeInorderTraversal {
         LinkedList<Integer> ret = new LinkedList<>();
         LinkedList<TreeNode> stack = new LinkedList();
         TreeNode cur = root;
-        while(!stack.isEmpty() || cur != null) {
+        while (!stack.isEmpty() || cur != null) {
             while (cur != null) {
                 stack.offerLast(cur);
                 cur = cur.left;
