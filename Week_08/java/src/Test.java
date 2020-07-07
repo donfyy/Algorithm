2ae8b137fccfc6bc7489b4aae2159c739e75a101
donfyy <sven.xinxin.wang@gmail.com>
2020-07-07 08:52:21 +0800

    bits oprand

---
+++ Week_08/java/src/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+    public static void main(String[] args) {
+        int x = 1;
+        System.out.println(x % 2 == 1);
+    }
+    public static void main1(String[] args) {
+        int x = 1;
+        System.out.println((x & 1) == 1);
+    }
+
+}
