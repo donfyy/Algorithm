122a4edb94b00ef375fa572ae42736eee8de8e25
donfyy <sven.xinxin.wang@gmail.com>
2020-07-09 07:47:16 +0800

    review

---
+++ Week_08/kotlin/src/_191_NumbersOf1Bits.kt
@@ -0,0 +1,10 @@
+// you need treat n as an unsigned value
+fun hammingWeight(n:Int):Int {
+    var count = 0
+    var m = n
+    while (m != 0) {
+        count++
+        m = m and m - 1
+    }
+    return count
+}
