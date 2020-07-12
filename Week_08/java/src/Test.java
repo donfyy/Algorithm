cd027eb00634f963c47b04d7899078b4a5373bda
donfyy <sven.xinxin.wang@gmail.com>
2020-07-13 07:45:23 +0800

    fix bug

--- Week_08/java/src/Test.java
+++ Week_08/java/src/Test.java
@@ -1,11 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-        int x = 1;
-        System.out.println(x % 2 == 1);
-    }
-    public static void main1(String[] args) {
-        int x = 1;
-        System.out.println((x & 1) == 1);
+        System.out.println(1 & (~(1 << 0)));
     }
 
 }
