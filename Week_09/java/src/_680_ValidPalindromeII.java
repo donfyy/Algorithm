ee1b231721c33d8a0b7daa6df118fa135813e719
donfyy <sven.xinxin.wang@gmail.com>
2020-07-18 11:59:13 +0800

    valid palindrome II

---
+++ Week_09/java/src/_680_ValidPalindromeII.java
@@ -0,0 +1,26 @@
+/**
+ * 第一遍：2020/07/18周六 ✅
+ * 第二遍：2020/07/16周三
+ * 第三遍：2020/07/08周四
+ * 第四遍：2020/07/13周一
+ * 第三遍：2020/06/29周一
+ * 第四遍：2020/07/05周日
+ */
+class _680_ValidPalindromeII {
+    public boolean validPalindrome(String s) {
+        if (s == null || s.length() == 0) return false;
+        return dfs(s, 0, s.length() - 1, false);
+    }
+
+    boolean dfs(String s, int l, int r, boolean deleted) {
+        while (l < r) {
+            if (s.charAt(l) != s.charAt(r)) {
+                if (deleted) return false;
+                return dfs(s, l + 1, r, true) || dfs(s, l, r - 1, true);
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
