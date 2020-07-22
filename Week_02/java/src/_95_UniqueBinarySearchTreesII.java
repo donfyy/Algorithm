1077aeecc165931dbb9ee686d8d37ed0b5260496
donfyy <sven.xinxin.wang@gmail.com>
2020-07-22 09:27:32 +0800

    review unique binary search trees II

--- Week_02/java/src/_95_UniqueBinarySearchTreesII.java
+++ Week_02/java/src/_95_UniqueBinarySearchTreesII.java
@@ -1,6 +1,7 @@
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+
 /**
  * 第一遍：2020/07/20周二 ✅
  * 第二遍：2020/07/21周二
@@ -8,8 +9,9 @@ import java.util.List;
  * 第四遍：2020/07/13周一
  * 第三遍：2020/06/29周一
  * 第四遍：2020/07/05周日
- * 今不知道回事，居然有理解障碍，卧槽
+ * 今不知道回事，居然有理解障碍，卧槽，可能是太累了吧，早上就没有，而且比较顺利地写出来了dp解法
  * 明天要复习下。
+ * 还有一种节省空间的dp解法
  * todo dp解法
  */
 class _95_UniqueBinarySearchTreesII {
@@ -34,6 +36,37 @@ class _95_UniqueBinarySearchTreesII {
             }
         }
         return trees;
+    }
+
+    public List<TreeNode> generateTreesDp(int n) {
+        // f(i, j) 表示(i, j)生成的二叉搜索树集合 (0, n + 1)
+        // f(i, j) = f(i, k) * f(k, j) for k in (i, j)
+        //  0 <= i <= n           1 <= j <= n + 1
+        // f(0, n + 1)
+        // if (j == i + 1)
+        if (n < 1) return Collections.emptyList();
+        List[][] dp = new List[n + 2][n + 2];
+        for (int i = n; i >= 0; i--) {
+            for (int j = i + 1; j <= n + 1; j++) {
+                LinkedList<TreeNode> list = new LinkedList<>();
+                if (j == i + 1) {
+                    list.add(null);
+                } else {
+                    for (int k = i + 1; k < j; k++) {
+                        LinkedList<TreeNode> leftTrees = (LinkedList<TreeNode>) dp[i][k];
+                        LinkedList<TreeNode> rightTrees = (LinkedList<TreeNode>) dp[k][j];
+
+                        for (TreeNode left : leftTrees) {
+                            for (TreeNode right : rightTrees) {
+                                list.add(new TreeNode(k, left, right));
+                            }
+                        }
+                    }
+                }
+                dp[i][j] = list;
+            }
+        }
+        return dp[0][n + 1];
     }
 
     public class TreeNode {
