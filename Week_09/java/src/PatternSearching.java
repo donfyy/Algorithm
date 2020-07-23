3e7d228e1917117a01c271b46f8bfe5308ded134
donfyy <sven.xinxin.wang@gmail.com>
2020-07-23 12:27:12 +0800

    kmp algorithm

--- Week_09/java/src/PatternSearching.java
+++ Week_09/java/src/PatternSearching.java
@@ -21,20 +21,61 @@ public class PatternSearching {
             }
         }
     }
-    // TODO: 2020/7/21 Rabin-Karp algorithm 
-    // TODO: 2020/7/21 KMP algorithm
+
+    // TODO: 2020/7/21 Rabin-Karp algorithm
     public static class KMP {
         public void search(String txt, String pat) {
-
+            int[] lps = createLPSArray(pat);
+            int m = txt.length();
+            int n = pat.length();
+            int i = 0;
+            int j = 0;
+            while (i < m) {
+                if (txt.charAt(i) == pat.charAt(j)) {
+                    if (j == n - 1) {
+                        System.out.println("Pattern Found at index " + (i - j));
+                        j = lps[j];
+                        continue;
+                    }
+                    i++;
+                    j++;
+                } else {
+                    if (j != 0) {
+                        j = lps[j];
+                    } else {
+                        i++;
+                    }
+                }
+            }
         }
 
-        public int[] createPrefixTable(String pat) {
-           int m = pat.length();
-           int[] prefix = new int[m];
-           prefix[0] = 0;
-           int len = 0;
-           int i = 1;
-           return prefix;
+        public static void main(String[] args) {
+            new KMP().search("AABAACAADAABAABA", "AABA");
+        }
+
+        public int[] createLPSArray(String pat) {
+            int n = pat.length();
+            int[] lps = new int[n];
+            lps[0] = -1;
+            lps[1] = 0;
+            //A   B A B C A B A A
+            //0   0 1 2 0 1 2 3
+            int len = 0;
+            int i = 1;
+            while (i < n - 1) {
+                if (pat.charAt(i) == pat.charAt(len)) {
+                    len++;
+                    lps[++i] = len;
+                } else {
+                    if (len > 0) {
+                        len = lps[len];
+                    } else {
+                        lps[++i] = 0;
+                    }
+                }
+            }
+
+            return lps;
         }
     }
 }
