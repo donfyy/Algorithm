94e663f79c3d9b748c62b8c530977c2a671bc1d2
donfyy <sven.xinxin.wang@gmail.com>
2020-07-27 07:51:39 +0800

    review decode ways

--- Week_06/kotlin/src/_91_DecodeWays.kt
+++ Week_06/kotlin/src/_91_DecodeWays.kt
@@ -1,9 +1,8 @@
 fun numDecodings(s: String): Int {
-    //f(i)前i个字符的解码方法数
-    //if (s[i - 1] == '0') if (s[i - 2] == '1' || s[i - 2] == '2') f(i) = f(i - 2) else f(i) =  0
-    //else if (s[i - 2] == '1' || (s[i - 2] == '2' && s[i - 1] <= '6')) f(i) = f(i - 1) + f(i - 2) else f(i) = f(i - 1)
-    //i in [1, s.size] f(0) = 1 f(1) = 1
-    //if (s[0] == '0') return 0
+    //f(i) 表示 前i个字符的解码方法总数
+    //如果s[i - 1]在[1, 9]之间则f(i) = f(i - 1)
+    //如果s[i - 2, i - 1]在[10, 26]之间 f(i) += f(i - 2)
+    //f(0) = 1, f(1) = 1 i in [2, s.length]  s[0] == 0 f(i) = 0
     if (s.isEmpty() || s[0] == '0') return 0
     var leftPreceding = 1
     var left = 1
@@ -20,5 +19,5 @@ fun numDecodings(s: String): Int {
         leftPreceding = left
         left = dp
     }
-    return left;
+    return left
 }
