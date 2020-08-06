c656682f4c5dc1606f63d05122a211270db7d918
donfyy <sven.xinxin.wang@gmail.com>
2020-08-06 14:36:51 +0800

    review course schedule

--- Week_02/java/src/_207_CourseSchedule.java
+++ Week_02/java/src/_207_CourseSchedule.java
@@ -11,26 +11,64 @@ import java.util.Queue;
  */
 class _207_CourseSchedule {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> edges = new ArrayList<>(numCourses);
+        for (int i = 0; i < numCourses; i++) {
+            edges.add(new ArrayList<>());
+        }
         int[] indegrees = new int[numCourses];
-        List<List<Integer>> adjacency = new ArrayList<>();
+        for (int[] pair : prerequisites) {
+            indegrees[pair[0]]++;
+            edges.get(pair[1]).add(pair[0]);
+        }
+
+        int n = numCourses;
         Queue<Integer> queue = new LinkedList<>();
         for (int i = 0; i < numCourses; i++) {
-            adjacency.add(new ArrayList<>());
-        }
-        for (int[] cp : prerequisites) {
-            indegrees[cp[0]]++;
-            adjacency.get(cp[1]).add(cp[0]);
-        }
-        for (int i = 0; i < numCourses; i++) {
-            if (indegrees[i] == 0) queue.add(i);
-        }
-        while (!queue.isEmpty()) {
-            int pre = queue.poll();
-            numCourses--;
-            for (int cur : adjacency.get(pre)) {
-                if (--indegrees[cur] == 0) queue.add(cur);
+            if (indegrees[i] == 0) {
+                queue.offer(i);
             }
         }
-        return numCourses == 0;
+        while (!queue.isEmpty()) {
+            int u = queue.poll();
+            for (int v : edges.get(u)) {
+                if (--indegrees[v] == 0) {
+                    queue.offer(v);
+                }
+            }
+            n--;
+        }
+        return n == 0;
+    }
+
+    class SolutionDfs {
+        public boolean canFinish(int numCourses, int[][] prerequisites) {
+            List<List<Integer>> edges = new ArrayList<>(numCourses);
+            for (int i = 0; i < numCourses; i++) {
+                edges.add(new ArrayList<>());
+            }
+            int[] visited = new int[numCourses];
+            for (int[] pair : prerequisites) {
+                edges.get(pair[1]).add(pair[0]);
+            }
+
+            for (int i = 0; i < numCourses; i++) {
+                if (!dfs(i, edges, visited)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        // true 表示从顶点u出发的图是DAG， false 表示从顶点u出发的图不是DAG，也就是找到了环。
+        boolean dfs(int u, List<List<Integer>> edges, int[] visited) {
+            if (visited[u] == 1) return false;
+            if (visited[u] == 2) return true;
+            visited[u] = 1;
+            for (int v : edges.get(u)) {
+                if (!dfs(v, edges, visited)) return false;
+            }
+            visited[u] = 2;
+            return true;
+        }
     }
 }
