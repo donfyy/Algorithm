6f9d792d14678cbfd62490316c887940baaf6527
donfyy <sven.xinxin.wang@gmail.com>
2020-08-06 08:15:00 +0800

    review flatten binary tree to linked list

--- Week_02/kotlin/src/_114_FlattenBinaryTreeToLinkedList.kt
+++ Week_02/kotlin/src/_114_FlattenBinaryTreeToLinkedList.kt
@@ -3,35 +3,39 @@ import java.util.*
 fun flatten(root: TreeNode?): Unit {
     var node = root
     while (node != null) {
-        node.left?.let { left ->
-            var pre = left
+        node.left?.let {
+            var pre = it
             while (pre.right != null) {
-                pre.right?.let { pre = it }
+                pre = pre.right!!
             }
             pre
-        }?.let { pre ->
-            pre.right = node?.right.also { node?.right = node?.left.also { node?.left = null } }
+        }?.let {
+            it.right = node?.right
+            node?.right = node?.left
+            node?.left = null
         }
         node = node.right
     }
 }
+
 class _114_PreorderIterative {
     fun flatten(root: TreeNode?): Unit {
         val stack = LinkedList<TreeNode>()
         root?.let { stack.push(it) }
-        var pre : TreeNode? = null
+        var pre: TreeNode? = null
         while (stack.isNotEmpty()) {
             val node = stack.pop()
-            pre?.let {
-                it.right = node
-                it.left = null
+            pre?.apply {
+                right = node
+                left = null
             }
             pre = node
             node.right?.let { stack.push(it) }
-            node.left?.let{ stack.push(it) }
+            node.left?.let { stack.push(it) }
         }
     }
 }
+
 class _114_PreorderRecursive {
     fun flatten(root: TreeNode?): Unit {
         dfs(root, null)
@@ -39,15 +43,16 @@ class _114_PreorderRecursive {
 
     fun dfs(node: TreeNode?, pre: TreeNode?): TreeNode? {
         return node?.let {
-            pre?.let {
-                it.left = null
-                it.right = node
+            pre?.apply {
+                left = null
+                right = node
             }
             val right = it.right
             dfs(right, dfs(it.left, it))
-        }?: pre
+        } ?: pre
     }
 }
+
 class _114_PostorderRecursive {
     fun flatten(root: TreeNode?): Unit {
         dfs(root, null)
@@ -57,9 +62,10 @@ class _114_PostorderRecursive {
         return node?.apply {
             right = dfs(left, dfs(right, next))
             left = null
-        }?:next
+        } ?: next
     }
 }
+
 class _114_PostorderIterative {
     fun flatten(root: TreeNode?): Unit {
         val stack = LinkedList<TreeNode>()
@@ -70,15 +76,14 @@ class _114_PostorderIterative {
                 stack.push(node)
                 node = node.right
             }
-
             val top = stack.peek()
-            if (top.left == null || top.left == next) {
+            if (top.left != null && top.left != next) {
+                node = top.left
+            } else {
                 stack.pop()
                 top.right = next
                 top.left = null
                 next = top
-            } else {
-                node = top.left
             }
         }
     }
