0748c179df3e17f6370424d9fc61a55045ff35d7
donfyy <sven.xinxin.wang@gmail.com>
2020-08-10 12:18:02 +0800

    count binary substrings

---
+++ Week_01/java/src/_696_CountBinarySubstrings.java
@@ -0,0 +1,25 @@
+/**
+ * 第一遍：2020/08/09周日 ✅
+ * 第二遍：2020/08/09周日
+ * 第三遍：2020/07/18周六
+ * 第三遍：2020/07/08周四
+ * 简单题没想出来啊，菜。。
+ */
+class _696_CountBinarySubstrings {
+    public int countBinarySubstrings(String s) {
+        if (s == null) return 0;
+        int ret = 0;
+        int pre = 0;
+        int curr = 1;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i - 1)) {
+                curr++;
+            } else {
+                ret += Math.min(curr, pre);
+                pre = curr;
+                curr = 1;
+            }
+        }
+        return ret + Math.min(curr, pre);
+    }
+}
