9f7230b489c997ec7ffc0bd368b42030683321ef
donfyy <sven.xinxin.wang@gmail.com>
2020-08-11 08:00:29 +0800

    recover binary search tree

--- Week_02/kotlin/src/_99_RecoveryBinarySearchTree.kt
+++ Week_02/kotlin/src/_99_RecoveryBinarySearchTree.kt
@@ -1,18 +1,18 @@
 import java.util.*
 
 fun recoverTree(root: TreeNode?): Unit {
-    val stack = LinkedList<TreeNode>()
     var node = root
+    var pre: TreeNode? = null
     var x: TreeNode? = null
     var y: TreeNode? = null
-    var pre: TreeNode? = null
+    val stack = LinkedList<TreeNode>()
     while (node != null || stack.isNotEmpty()) {
         while (node != null) {
             stack.push(node)
             node = node.left
         }
         node = stack.pop()
-        if (pre != null && pre.`val` > node?.`val`!!) {
+        if (pre != null && pre.`val` > node.`val`) {
             y = node
             if (x == null) {
                 x = pre
@@ -23,29 +23,38 @@ fun recoverTree(root: TreeNode?): Unit {
         pre = node
         node = node.right
     }
-
-    x?.`val` = y?.`val`?.also { y.`val` = x?.`val`!! }!!
+    if (x != null && y != null) {
+        x.`val` = y.`val`.also { y.`val` = x.`val` }
+    }
 }
 
 class SolutionMorris {
     fun recoverTree(root: TreeNode?): Unit {
+        var node = root
         var x: TreeNode? = null
         var y: TreeNode? = null
         var pre: TreeNode? = null
-        var node = root
         while (node != null) {
-            if (node.left != null) {
-                var predecessor = node.left
-                while (predecessor?.right != null && predecessor.right != node) {
-                    predecessor = predecessor.right
+            if (node.left == null) {
+                if (pre != null && pre.`val` > node.`val`) {
+                    y = node
+                    if (x == null) {
+                        x = pre
+                    }
+                }
+                pre = node
+                node = node.right
+            } else {
+                var p = node.left
+                while (p?.right != null && p.right != node) {
+                    p = p.right
                 }
 
-                if (predecessor?.right == null) {
-                    predecessor?.right = node
+                if (p?.right == null) {
+                    p?.right = node
                     node = node.left
                 } else {
-                    predecessor.right = null
-                    // process root logic
+                    p.right = null
                     if (pre != null && pre.`val` > node.`val`) {
                         y = node
                         if (x == null) {
@@ -55,19 +64,11 @@ class SolutionMorris {
                     pre = node
                     node = node.right
                 }
-            } else {
-                // process root logic
-                if (pre != null && pre.`val` > node.`val`) {
-                    y = node
-                    if (x == null) {
-                        x = pre
-                    }
-                }
-                pre = node
-                node = node.right
             }
         }
 
-        x?.`val` = y?.`val`?.also { y.`val` = x?.`val`!! }!!
+        if (x != null && y != null) {
+            x.`val` = y.`val`.also { y.`val` = x.`val` }
+        }
     }
 }
