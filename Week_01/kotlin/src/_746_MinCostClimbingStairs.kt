eed23a5744d2b8629b4a65015ad226fd7fe88f35
donfyy <sven.xinxin.wang@gmail.com>
2020-08-13 12:04:28 +0800

    climbing stairs

--- Week_01/kotlin/src/_746_MinCostClimbingStairs.kt
+++ Week_01/kotlin/src/_746_MinCostClimbingStairs.kt
@@ -1,8 +1,8 @@
 fun minCostClimbingStairs(cost: IntArray): Int {
-    //f(n)到达第n层的最低花费
-    //f(n) = min(f(n - 1), f(n - 2)) + cost[n]
+    //f(i)从i层起跳的最低花费
+    //f(i) = min(f(i - 1), f(i - 2)) + cost[i]
     //min(f(n - 1), f(n - 2))
-    //f(0) = coust[0] f(1) = coust[1]
+    //f(0) = cost[0] f(1) = cost[1]
     var p = 0
     var q = 0
     for (v in cost) {
