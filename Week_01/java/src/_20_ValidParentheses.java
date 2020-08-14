d763382770850f32e0b97b51d272e5e4a7fd823d
donfyy <sven.xinxin.wang@gmail.com>
2020-08-14 23:41:41 +0800

    valid paratheses

--- Week_01/java/src/_20_ValidParentheses.java
+++ Week_01/java/src/_20_ValidParentheses.java
@@ -1,7 +1,16 @@
 import java.util.LinkedList;
 
+/**
+ * 第一遍：2020/08/13周四 ✅
+ * 第二遍：2020/08/14周五 ✅
+ * 第三遍：2020/07/27周一
+ * 第四遍：2020/07/28周二
+ * 第五遍：2020/08/09周日
+ * 第六遍：2020/08/09周一
+ * 第七遍：2020/08/12周三
+ */
 class _20_ValidParentheses {
-    
+
     public boolean isValid(String s) {
         if (s == null) {
             return false;
@@ -28,5 +37,5 @@ class _20_ValidParentheses {
 
         return stack.isEmpty();
     }
-    
+
 }
---
+++ Week_01/kotlin/src/_20_ValidParentheses.kt
@@ -0,0 +1,14 @@
+import java.util.*
+
+fun isValid(s: String): Boolean {
+    val stack = LinkedList<Char>()
+    for (c in s) {
+        when (c) {
+            '(' -> stack.push(')')
+            '{' -> stack.push('}')
+            '[' -> stack.push(']')
+            else -> if (stack.isEmpty() || stack.pop() != c) return false
+        }
+    }
+    return stack.isEmpty()
+}
