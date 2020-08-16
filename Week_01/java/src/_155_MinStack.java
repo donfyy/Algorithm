7552fb279b0c1b537e95c7b38d7445f8183c8c0e
donfyy <sven.xinxin.wang@gmail.com>
2020-08-16 14:46:06 +0800

    valid paratheses

--- Week_01/java/src/_155_MinStack.java
+++ Week_01/java/src/_155_MinStack.java
@@ -1,32 +1,53 @@
 import java.util.LinkedList;
 
+/**
+ * 第一遍：2020/08/13周四 ✅
+ * 第二遍：2020/08/14周五 ✅
+ * 第三遍：2020/08/16周日 ✅
+ * 第四遍：2020/07/28周二
+ * 第五遍：2020/08/09周日
+ * 第六遍：2020/08/09周一
+ * 第七遍：2020/08/12周三
+ */
 class _155_MinStack {
-    
-    public boolean isValid(String s) {
-        if (s == null) {
-            return false;
-        }
-        if (s.length() == 0) {
-            return true;
+    class MinStack {
+        LinkedList<Integer> stack = new LinkedList<>();
+        LinkedList<Integer> minStack = new LinkedList<>();
+
+        /**
+         * initialize your data structure here.
+         */
+        public MinStack() {
+
         }
 
-        LinkedList<Character> stack = new LinkedList<>();
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (c == '(') {
-                stack.offerLast(')');
-            } else if (c == '[') {
-                stack.offerLast(']');
-            } else if (c == '{') {
-                stack.offerLast('}');
-            } else {
-                if (stack.isEmpty() || stack.pollLast() != c) {
-                    return false;
-                }
+        public void push(int x) {
+            stack.push(x);
+            if (!minStack.isEmpty()) {
+                x = Math.min(x, minStack.peek());
+            }
+            minStack.push(x);
+        }
+
+        public void pop() {
+            if (!stack.isEmpty()) {
+                stack.pop();
+                minStack.pop();
             }
         }
 
-        return stack.isEmpty();
+        public int top() {
+            if (!stack.isEmpty()) {
+                return stack.peek();
+            }
+            return -1;
+        }
+
+        public int getMin() {
+            if (!minStack.isEmpty()) {
+                return minStack.peek();
+            }
+            return -1;
+        }
     }
-    
 }
