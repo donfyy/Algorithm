10097a78b580daacba62fd130e19a68ebcfe9c99
donfyy <sven.xinxin.wang@gmail.com>
2020-08-17 13:48:11 +0800

    trapping rain water

--- Week_01/kotlin/src/_42_TrappingRainWater.kt
+++ Week_01/kotlin/src/_42_TrappingRainWater.kt
@@ -3,7 +3,7 @@ import java.util.*
 fun trap(height: IntArray): Int {
     if (height.size < 3) return 0
     var l = 0
-    var r = height.size - 1
+    var r = height.lastIndex
     var leftMax = 0
     var rightMax = 0
     var ret = 0
@@ -34,12 +34,12 @@ fun trap1(height: IntArray): Int {
     for (i in height.indices) {
         val curr = height[i]
         //少了一个等号，速度提升了30ms，kotlin太慢
-        while (!stack.isEmpty() && height[stack.peekLast()] < curr) {
-            val topIdx = stack.pollLast()
+        while (!stack.isEmpty() && height[stack.peek()] < curr) {
+            val topIdx = stack.poll()
             if (stack.isEmpty()) break
-            ret += (Math.min(height[stack.peekLast()], curr) - height[topIdx]) * (i - stack.peekLast() - 1)
+            ret += (minOf(height[stack.peek()], curr) - height[topIdx]) * (i - stack.peek() - 1)
         }
-        stack.offerLast(i)
+        stack.push(i)
     }
     return ret
 }
