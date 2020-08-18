b704363679577b6ee9fa05abae615093ea89a1f8
donfyy <sven.xinxin.wang@gmail.com>
2020-08-19 07:38:23 +0800

    balanced binary tree

--- Week_02/kotlin/src/_110_BalancedBinaryTee.kt
+++ Week_02/kotlin/src/_110_BalancedBinaryTee.kt
@@ -1,12 +1,14 @@
 fun isBalanced(root: TreeNode?): Boolean {
-    fun depth(root: TreeNode?): Int {
-        if (root == null) return 0
-        val l = depth(root.left)
-        val r = depth(root.right)
-        if (l == -1 || r == -1 || Math.abs(l - r) > 1) {
-            return -1
+    fun depth(node: TreeNode?): Int {
+        if (node == null) return 0
+        val l = depth(node.left)
+        val r = depth(node.right)
+        return if (l == -1 || r == -1 || Math.abs(r - l) > 1) {
+            -1
+        } else {
+            maxOf(l, r) + 1
         }
-        return maxOf(l, r) + 1
     }
+
     return depth(root) != -1
 }
