8a9d1f017761a6e0a5cd0875b35ec793e98c2339
donfyy <sven.xinxin.wang@gmail.com>
2020-08-24 09:49:30 +0800

    hamming distance

--- Week_08/kotlin/src/countingbits/_201_BitwiseAndOfNumbersRange.kt
+++ Week_08/kotlin/src/countingbits/_201_BitwiseAndOfNumbersRange.kt
@@ -7,3 +7,17 @@ fun rangeBitwiseAnd(m: Int, n: Int): Int {
     }
     return ret
 }
+
+class _201_Solution_ {
+    fun rangeBitwiseAnd(m: Int, n: Int): Int {
+        var a = m
+        var b = n
+        var shift = 0
+        while (a != b) {
+            a = a shr 1
+            b = b shr 1
+            shift++
+        }
+        return a shl shift
+    }
+}
