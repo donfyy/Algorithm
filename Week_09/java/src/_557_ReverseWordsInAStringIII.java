bb476120459173cc80389c02358dffcb6af47dec
donfyy <sven.xinxin.wang@gmail.com>
2020-08-30 09:44:04 +0800

    reverse words in a string iii

--- Week_09/java/src/_557_ReverseWordsInAStringIII.java
+++ Week_09/java/src/_557_ReverseWordsInAStringIII.java
@@ -1,31 +1,27 @@
 /**
  * 第一遍：2020/07/15周三 ✅
  * 第二遍：2020/07/16周四 ✅
- * 第三遍：2020/07/08周四
+ * 第三遍：2020/08/30周日 ✅
  * 第四遍：2020/07/13周一
  * 第三遍：2020/06/29周一
  * 第四遍：2020/07/05周日
  */
 class _557_ReverseWordsInAStringIII {
     public String reverseWords(String s) {
-        if (s == null || s.length() == 0) return s;
-        char[] array = s.toCharArray();
-        int left = 0, right = 0, length = array.length;
-        while (left < length) {
-            while (right < length && array[right] != ' ') right++;
-            reverse(array, left, right - 1);
-            left = ++right;
+        if (s == null || s.isEmpty()) return s;
+        char[] arr = s.toCharArray();
+        int i = 0, j = 0, n = arr.length;
+        while (i < n) {
+            while (j < n && arr[j] != ' ') j++;
+            int next = j-- + 1;
+            while (i < j) {
+                char c = arr[i];
+                arr[i++] = arr[j];
+                arr[j--] = c;
+            }
+            i = next;
+            j = next;
         }
-        return String.valueOf(array);
-    }
-
-    void reverse(char[] array, int left, int right) {
-        while (left < right) {
-            char temp = array[left];
-            array[left] = array[right];
-            array[right] = temp;
-            left++;
-            right--;
-        }
+        return new String(arr);
     }
 }
