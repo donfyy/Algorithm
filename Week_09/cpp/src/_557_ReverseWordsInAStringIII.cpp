f64dc5185c9c018675e37811541c3d9fb1c85b72
donfyy <sven.xinxin.wang@gmail.com>
2020-09-03 20:39:09 +0800

    add more

--- Week_09/cpp/src/_557_ReverseWordsInAStringIII.cpp
+++ Week_09/cpp/src/_557_ReverseWordsInAStringIII.cpp
@@ -1,12 +1,20 @@
-class _557_ {
+#include <string>
+using namespace std;
+class _557_
+{
 public:
-    string reverseWords(string s) {
+    string reverseWords(string s)
+    {
         int i = 0, j = 0, n = s.length();
-        while (i < n) {
-            while (s[j] && s[j] != ' ') j++;
+        while (i < n)
+        {
+            while (s[j] && s[j] != ' ')
+                j++;
             int next = j-- + 1;
-            while (i < j) swap(s[i++], s[j--]);
-            i = next; j = next;
+            while (i < j)
+                swap(s[i++], s[j--]);
+            i = next;
+            j = next;
         }
         return s;
     }
---
+++ Week_09/cpp/src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    cout << "hello cpp!" << endl;
+    return 0;
+}
+
+void fun1(int& a) {
+
+}
