c1f45d791a07f8930b1ddf9b000d8122efbb8e8f
donfyy <sven.xinxin.wang@gmail.com>
2020-09-28 08:44:04 +0800

    assign cookies

---
+++ Week_04/kotlin/src/_455_AssignCookies.kt
@@ -0,0 +1,17 @@
+class _455_Greedy {
+    fun findContentChildren(g: IntArray, s: IntArray): Int {
+        g.sort()
+        s.sort()
+        var i = 0
+        var j = 0
+        while (i < g.size && j < s.size) {
+            if (g[i] <= s[j]) {
+                i++
+                j++
+            } else {
+                j++
+            }
+        }
+        return i
+    }
+}
