0a001f739ffe84b7925517132de167a3429db235
donfyy <sven.xinxin.wang@gmail.com>
2020-10-26 17:18:15 +0800

    maximum subarray

--- Week_06/kotlin/src/_53_MaximumSubarray.kt
+++ Week_06/kotlin/src/_53_MaximumSubarray.kt
@@ -1,12 +1,16 @@
-fun maxSubArray(nums: IntArray): Int {
-    //f(i) 以第i个元素为结尾的连续子数组的最大和
-    //f(i) = max(f(i - 1), 0) + value(i)
-    if (nums.isEmpty()) return 0
-    var ret = nums[0]
-    var dp = nums[0]
-    for (i in 1..nums.size - 1) {
-        dp = Math.max(dp, 0) + nums[i]
-        ret = Math.max(ret, dp)
+class _53_ {
+    class Solution {
+        fun maxSubArray(nums: IntArray): Int {
+            // f(i) 以i为结尾的连续子数组的最大和
+            // f(i) = max(f(i - 1), 0) + value(i)
+            // f(0) = 0
+            var f = Integer.MIN_VALUE
+            var ret = f
+            for (v in nums) {
+                f = maxOf(f, 0) + v
+                ret = maxOf(ret, f)
+            }
+            return ret
+        }
     }
-    return ret
 }
