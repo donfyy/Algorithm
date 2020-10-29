fd80b9019ad0a73ee4a034f8ca727f26b2092421
donfyy <sven.xinxin.wang@gmail.com>
2020-10-29 15:33:56 +0800

    how many numbers are smaller than the current number

--- Week_08/cpp/src/_1365_HowManyNumbersAreSmallerThanTheCurrentNumber.cpp
+++ Week_08/cpp/src/_1365_HowManyNumbersAreSmallerThanTheCurrentNumber.cpp
@@ -1,6 +1,6 @@
 #include <vector>
 using namespace std;
-class UsingCountingSort
+class UsingQuickSort
 {
 public:
     vector<int> smallerNumbersThanCurrent(vector<int> &nums)
@@ -24,5 +24,28 @@ public:
         }
         return ret;
         ;
+    }
+};
+class UsingCountingSort
+{
+public:
+    // O(N + K) O(K)
+    vector<int> smallerNumbersThanCurrent(vector<int> &nums)
+    {
+        // 比当前数字小的数字可以在当前数字的左侧也可以在当前数字的右侧
+        // 如果不对原数组进行处理，为每个数字扫描一次数组，时间O(n^2)
+        // 如果比当前数字小的数字都在当前数字的一侧就可以，就可以直接求出数字的数目
+        // 排序的时间复杂度是O(nlogn) 小于 O(n^2)
+        // 由于数字的取值范围有限，所以可以想到用计数排序
+        const int n = nums.size(), m = 101;
+        vector<int> bucket(m);
+        for (int num : nums)
+            bucket[num]++;
+        for (int i = 1; i < m; i++)
+            bucket[i] += bucket[i - 1];
+        vector<int> ret(n);
+        for (int i = 0; i < n; i++)
+            ret[i] = nums[i] == 0 ? 0 : bucket[nums[i] - 1];
+        return ret;
     }
 };
