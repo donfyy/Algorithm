68449807dfa5263f8b16e87f2edf7ef8926e8986
donfyy <sven.xinxin.wang@gmail.com>
2020-10-29 14:22:57 +0800

    subarray sum equals k

--- Week_01/cpp/src/_560_SubarraySumEqualsK.cpp
+++ Week_01/cpp/src/_560_SubarraySumEqualsK.cpp
@@ -10,16 +10,25 @@ public:
     // 将求和为k的连续子数组个数转换为求差为k的前缀和
     int subarraySum(vector<int> &nums, int k)
     {
-        unordered_map<int, int> t;
-        int cnt = 0, n = nums.size(), sum = 0;
-        t[0] = 1;
+        // 将连续子数组的和转化为两个前缀和的差
+        // 两个前缀和的差，又可以利用两数之和的思想
+        // f(i) 前i个数的和
+        // f(j) = f(i) - k  j in [0, i - 1]
+        // 给定f(i) 查找f(j) = f(i) - k
+        const int n = nums.size();
+        int pre = 0, ret = 0;
+        unordered_map<int, int> freq;
+        freq[0] = 1;
         for (int i = 0; i < n; i++)
         {
-            sum += nums[i];
-            if (t.count(sum - k))
-                cnt += t[sum - k];
-            t[sum]++;
+            pre += nums[i];
+            int other = pre - k;
+            if (freq.count(other))
+            {
+                ret += freq[other];
+            }
+            freq[pre]++;
         }
-        return cnt;
+        return ret;
     }
 };
