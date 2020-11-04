3315124b47a4d80e5cd791387186693538b01b8a
donfyy <sven.xinxin.wang@gmail.com>
2020-11-04 15:20:01 +0800

    merge intervals

--- Week_08/kotlin/src/_56_MergeIntervals.kt
+++ Week_08/kotlin/src/_56_MergeIntervals.kt
@@ -1,20 +1,16 @@
-import java.util.*
-
 fun merge(intervals: Array<IntArray>): Array<IntArray> {
-    if (intervals.isEmpty()) return intervals
-    Arrays.sort(intervals) { l, r -> l[0] - r[0] }
-    val ret = Array<IntArray?>(intervals.size) { null }
-    ret[0] = intervals[0]
-    var last = intervals[0]
-    var j = 1
-    for (i in 1 until intervals.size) {
-        val current = intervals[i]
-        if (current[0] <= last[1]) {
-            last[1] = Math.max(last[1], current[1])
+    if (intervals.isEmpty()) return emptyArray()
+    intervals.sortBy { it[0] }
+    val ret = mutableListOf<IntArray>()
+    var curr = intervals[0]
+    intervals.forEach {
+        if (it[0] > curr[1]) {
+            ret += curr
+            curr = it
         } else {
-            ret[j++] = current
-            last = current
+            curr[1] = maxOf(curr[1], it[1])
         }
     }
-    return Arrays.copyOf(ret, j)
+    ret += curr
+    return ret.toTypedArray()
 }
