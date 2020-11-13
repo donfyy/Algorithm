b82bb1b21c594578b046b2d09433bd9c32d6c04d
donfyy <sven.xinxin.wang@gmail.com>
2020-11-13 09:47:11 +0800

    count of range sum

--- Week_08/cpp/src/_327_CountOfRangeSum.cpp
+++ Week_08/cpp/src/_327_CountOfRangeSum.cpp
@@ -63,3 +63,60 @@ public:
         return ret;
     }
 };
+class UsingMergeSort2
+{
+public:
+    int countRangeSum(vector<int> &nums, int lower, int upper)
+    {
+        // 区间和->前缀和之差
+        vector<long> pre{0};
+        for (int it : nums)
+            pre.push_back(pre.back() + it);
+        const int n = pre.size();
+        vector<long> cache(n);
+        return merge(pre, 0, n - 1, lower, upper, cache);
+    }
+
+    int merge(vector<long> &pre, int left, int right, int lower, int upper, vector<long> &cache)
+    {
+        if (left >= right)
+            return 0;
+        int mid = left + ((right - left) >> 1);
+        int ret = merge(pre, left, mid, lower, upper, cache);
+        ret += merge(pre, mid + 1, right, lower, upper, cache);
+
+        // 注意训练逻辑推理能力
+        // pre[j] - pre[i] in [lower, upper]
+        // j - i >= lower  j - i <= upper
+        // j - upper <= i <= j - lower
+        // lower + i <= j <= i + upper
+        int l = left, r = l;
+        for (int j = mid + 1; j <= right; j++)
+        {
+            while (l <= mid && pre[j] - upper > pre[l])
+                l++;
+            while (r <= mid && pre[j] - lower >= pre[r])
+                r++;
+            ret += r - l;
+        }
+        // int l = mid + 1, r = l;
+        // for (int i = left; i <= mid; i++) {
+        //     while (l <= right && pre[l] < lower + pre[i]) l++;
+        //     while (r <= right && pre[r] <= upper + pre[i]) r++;
+        //     ret += r - l;
+        // }
+
+        // merge
+        int i = left, j = mid + 1, k = left;
+        while (i <= mid)
+        {
+            while (j <= right && pre[j] < pre[i])
+                cache[k++] = pre[j++];
+            cache[k++] = pre[i++];
+        }
+        while (j <= right)
+            cache[k++] = pre[j++];
+        copy(cache.begin() + left, cache.begin() + right + 1, pre.begin() + left);
+        return ret;
+    }
+};
