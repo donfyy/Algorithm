1b45c6fcc63310bd3bfcdd81dd6df72d7a95ff2c
donfyy <sven.xinxin.wang@gmail.com>
2020-11-23 11:51:33 +0800

    counting bits

--- Week_08/cpp/src/_338_CountingBits.cpp
+++ Week_08/cpp/src/_338_CountingBits.cpp
@@ -3,14 +3,19 @@ using namespace std;
 class Solution
 {
 public:
-    vector<int> countBits(int num)
+    vector<int> countBits(int n)
     {
-        if (num < 0)
-            return {};
-        vector<int> dp(num + 1);
-        for (int i = 1; i <= num; i++)
+        // [0, n]每一个数的二进制位中1的个数
+        // f(i) 表示i二进制位中1的个数
+        // f(i) = f(i & i - 1) + 1
+        // f(i) = f(i >> 1) + (i & 1)
+        // 无后效性对于dp来说非常精辟
+        // f(0) = 0
+        vector<int> dp(n + 1);
+        for (int i = 1; i <= n; i++)
         {
             dp[i] = dp[i >> 1] + (i & 1);
+            // dp[i] = dp[i & i - 1] + 1;
         }
         return dp;
     }
