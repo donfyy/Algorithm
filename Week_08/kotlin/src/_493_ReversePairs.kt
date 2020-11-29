c7e0e4f2431354d7183850744c7eec711b56f5f2
donfyy <sven.xinxin.wang@gmail.com>
2020-11-29 16:52:06 +0800

    reverse pairs

--- Week_08/kotlin/src/_493_ReversePairs.kt
+++ Week_08/kotlin/src/_493_ReversePairs.kt
@@ -1,59 +1,60 @@
-fun reversePairs(nums: IntArray): Int {
-    if (nums.size < 2) return 0
-    return mergeSort(nums, 0, nums.size - 1, IntArray(nums.size))
-}
-
-fun mergeSort(nums: IntArray, left:Int, right:Int, cache:IntArray):Int {
-    if (left >= right) return 0
-    val mid = left + ((right - left) shr 1)
-    var count = 0
-    count += mergeSort(nums, left, mid, cache)
-    count += mergeSort(nums, mid + 1, right, cache)
-
-    var i = left
-    var j = mid + 1
-    var k = left
-    var l = j
-
-    while (i <= mid) {
-        while (l <= right && nums[i] > 2L * nums[l]) l++
-        count += l - mid - 1
-
-        while (j <= right && nums[j] < nums[i]) cache[k++] = nums[j++]
-        cache[k++] = nums[i++]
+class _493_ {
+    class Solution {
+        fun reversePairs(nums: IntArray): Int {
+            val n = nums.size
+            val cache = IntArray(n)
+            fun merge(left: Int, right: Int): Int {
+                if (left >= right) return 0
+                val mid = (left + right) ushr 1
+                var ret = merge(left, mid)
+                ret += merge(mid + 1, right)
+                var i = left
+                var j = mid + 1
+                var k = j
+                var l = left
+                while (i <= mid) {
+                    while (j <= right && nums[j] * 2L < nums[i]) j++;
+                    ret += j - mid - 1;
+                    while (k <= right && nums[k] < nums[i]) cache[l++] = nums[k++];
+                    cache[l++] = nums[i++];
+                }
+                while (k <= right) cache[l++] = nums[k++];
+                System.arraycopy(cache, left, nums, left, right - left + 1);
+                return ret
+            }
+            return merge(0, n - 1);
+        }
     }
 
-    while (j <= right) cache[k++] = nums[j++]
-    System.arraycopy(cache, left, nums, left, right - left + 1)
-    return count
-}
-class SolutionRight {
-    fun reversePairs(nums: IntArray): Int {
-        if (nums.size < 2) return 0
-        return mergeSort(nums, 0, nums.size - 1, IntArray(nums.size))
-    }
-    fun mergeSort(nums:IntArray, left:Int, right:Int, cache:IntArray) :Int{
-        if (left >= right) return 0
-        val mid = left + ((right - left) shr 1)
-        var count = 0
-        count += mergeSort(nums, left, mid, cache)
-        count += mergeSort(nums, mid + 1, right, cache)
-
-        var i = left
-        var j = mid + 1
-        var k = left
-        var l = i
-
-        while (j <= right) {
-            while (l <= mid && nums[l] <= 2L * nums[j]) l++
-            count += mid + 1 - l
-
-            while (i <= mid && nums[i] < nums[j]) cache[k++] = nums[i++]
-            cache[k++] = nums[j++]
+    class SolutionRight {
+        fun reversePairs(nums: IntArray): Int {
+            if (nums.size < 2) return 0
+            return mergeSort(nums, 0, nums.size - 1, IntArray(nums.size))
         }
 
-        while (i <= mid) cache[k++] = nums[i++]
-        System.arraycopy(cache, left, nums, left, right - left + 1)
-        return count
+        fun mergeSort(nums: IntArray, left: Int, right: Int, cache: IntArray): Int {
+            if (left >= right) return 0
+            val mid = left + ((right - left) shr 1)
+            var count = 0
+            count += mergeSort(nums, left, mid, cache)
+            count += mergeSort(nums, mid + 1, right, cache)
+
+            var i = left
+            var j = mid + 1
+            var k = left
+            var l = i
+
+            while (j <= right) {
+                while (l <= mid && nums[l] <= 2L * nums[j]) l++
+                count += mid + 1 - l
+
+                while (i <= mid && nums[i] < nums[j]) cache[k++] = nums[i++]
+                cache[k++] = nums[j++]
+            }
+
+            while (i <= mid) cache[k++] = nums[i++]
+            System.arraycopy(cache, left, nums, left, right - left + 1)
+            return count
+        }
     }
 }
