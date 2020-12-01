833c2d6b8c9b109d2ff19d513ef3e15a1e7128a5
donfyy <sven.xinxin.wang@gmail.com>
2020-12-01 09:59:17 +0800

    count complete tree nodes

--- Week_04/cpp/src/_222_CountCompleteTreeNodes.cpp
+++ Week_04/cpp/src/_222_CountCompleteTreeNodes.cpp
@@ -58,3 +58,89 @@ public:
         return root;
     }
 };
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class UsingRecur
+{
+public:
+    // O((logn)^2) O(logn)
+    int countNodes(TreeNode *root)
+    {
+        if (!root)
+            return 0;
+        auto height = [](TreeNode *node) -> int {
+            int ret = 0;
+            while (node)
+            {
+                node = node->left;
+                ret++;
+            }
+            return ret;
+        };
+        int l = height(root->left);
+        int r = height(root->right);
+        if (l == r)
+            return (1 << l) + countNodes(root->right);
+        return countNodes(root->left) + (1 << r);
+    }
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class UsingBinarySearch2
+{
+public:
+    int countNodes(TreeNode *root)
+    {
+        // 假设树的高度为h 则节点数在[2^(h - 1), 2^h)中
+        // 注意到第k个节点（从1开始）的二进制位表示恰好是从跟节点到其的路径
+        // 因此我们可以使用二分查找的方式找到完全二叉树的节点数
+        int h = 0;
+        auto node = root;
+        while (node)
+        {
+            node = node->left;
+            h++;
+        }
+        if (h < 2)
+            return h;
+        auto exists = [&](int k) -> bool {
+            int level = 1 << (h - 2);
+            auto node = root;
+            while (level)
+            {
+                node = level & k ? node->right : node->left;
+                level >>= 1;
+            }
+            return node;
+        };
+        int l = 1 << (h - 1), r = 1 << h;
+        while (l < r)
+        {
+            int m = l + ((r - l) >> 1);
+            if (exists(m))
+            {
+                l = m + 1;
+            }
+            else
+            {
+                r = m;
+            }
+        }
+        return l - 1;
+    }
+};
