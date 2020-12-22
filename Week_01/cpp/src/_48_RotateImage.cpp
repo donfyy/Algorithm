674685974887ccf46ae9b86b81bd25a74f5806fe
donfyy <sven.xinxin.wang@gmail.com>
2020-12-22 08:26:53 +0800

    rotate image

--- Week_01/cpp/src/_48_RotateImage.cpp
+++ Week_01/cpp/src/_48_RotateImage.cpp
@@ -47,7 +47,7 @@ public:
     void rotate(vector<vector<int>> &matrix)
     {
         const int n = matrix.size();
-        for (int i = 0; i<n> > 1; i++)
+        for (int i = 0; i< n>> 1; i++)
         {
             for (int j = 0; j < n; j++)
             {
